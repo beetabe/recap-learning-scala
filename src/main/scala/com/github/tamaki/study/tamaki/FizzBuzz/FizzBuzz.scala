612cecbc192e38d487382d5e95c953fba94a939c
ryosuke <ryosuke.tabe@bizreach.co.jp>
2015-02-09 11:41:28 +0900

    FizzBuzz問題解いた

--- src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
@@ -4,7 +4,34 @@ package com.github.tamaki.study.tamaki.FizzBuzz
  * Created by tamaki on 2015/02/08.
  */
 object FizzBuzz {
+  /*
+   * 1 ~ 100 まで
+   * 3 の倍数は Fizz
+   * 5 の倍数は Buzz
+   * 15 の倍数は FizzBuzz
+   * それ以外はそのまま数字を。
+   */
   def main(args: Array[String]): Unit = {
-
+    (1 to 100).foreach( n => {
+      val fb = n % 15
+      val b = n % 5
+      val f = n % 3
+      fb match {
+        case 0 =>
+          println("FizzBuzz")
+        case _ =>
+          b match {
+            case 0 =>
+              println("Buzz")
+            case _ =>
+              f match {
+                case 0 =>
+                  println("Fizz")
+                case _ =>
+                  println(n)
+              }
+          }
+      }
+   })
   }
 }
